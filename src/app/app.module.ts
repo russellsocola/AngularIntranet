aadef49de6361afb38bc7e0bf63b090d35ef9103
russellsocola <russell999nahum@gamil.com>
2024-06-19 16:58:22 -0500

    "Updated app.component.html to comment out app-portafolio and add app-items, and added AboutComponent and ItemsComponent to app.module.ts declarations"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
